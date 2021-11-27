ec0b4e45bfad5c45c953ba8c648afde34e08302f
Akira Hayakawa <ruby.wktk@gmail.com>
2021-11-28 00:01:50 +0900

    Add test for server-streaming

--- example/streaming/tests/streaming.rs
+++ example/streaming/tests/streaming.rs
@@ -8,9 +8,9 @@ trait BidiStreaming {
     fn double(input: Stream<u64>) -> Stream<u64>;
 }
 #[derive(Clone)]
-struct BidiApp;
+struct BidiStreamingApp;
 #[norpc::async_trait]
-impl BidiStreaming for BidiApp {
+impl BidiStreaming for BidiStreamingApp {
     async fn double(
         self,
         st: Pin<Box<(dyn futures::Stream<Item = u64> + std::marker::Send + 'static)>>,
@@ -28,7 +28,7 @@ impl BidiStreaming for BidiApp {
 async fn test_bidi_streaming() {
     let (tx, rx) = mpsc::unbounded_channel();
     tokio::spawn(async move {
-        let app = BidiApp;
+        let app = BidiStreamingApp;
         let service = BidiStreamingService::new(app);
         let server = norpc::ServerChannel::new(rx, service);
         server.serve().await
@@ -40,4 +40,42 @@ async fn test_bidi_streaming() {
     assert_eq!(ret_stream.next().await, Some(2));
     assert_eq!(ret_stream.next().await, Some(4));
     ret_stream.next().await;
+}
+
+
+#[norpc::service]
+trait ServerStreaming {
+    fn double(input: u64) -> Stream<u64>;
+}
+#[derive(Clone)]
+struct ServerStreamingApp;
+#[norpc::async_trait]
+impl ServerStreaming for ServerStreamingApp {
+    async fn double(
+        self,
+        x: u64,
+    ) -> Pin<Box<(dyn futures::Stream<Item = u64> + std::marker::Send + 'static)>> {
+        let st = async_stream::stream! {
+            for i in x..x+3 {
+                yield i*2;
+            }
+        };
+        Box::pin(st)
+    }
+}
+#[tokio::test(flavor = "multi_thread")]
+async fn test_server_streaming() {
+    let (tx, rx) = mpsc::unbounded_channel();
+    tokio::spawn(async move {
+        let app = ServerStreamingApp;
+        let service = ServerStreamingService::new(app);
+        let server = norpc::ServerChannel::new(rx, service);
+        server.serve().await
+    });
+    let chan = norpc::ClientChannel::new(tx);
+    let mut cli = ServerStreamingClient::new(chan);
+    let mut ret_stream = cli.double(3).await.unwrap();
+    assert_eq!(ret_stream.next().await, Some(6));
+    assert_eq!(ret_stream.next().await, Some(8));
+    assert_eq!(ret_stream.next().await, Some(10));
 }
