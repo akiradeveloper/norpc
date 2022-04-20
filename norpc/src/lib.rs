ec7e76831c0a767effa24ecf2ccd8d4652ebf5e1
Akira Hayakawa <ruby.wktk@gmail.com>
2022-04-20 14:44:49 +0900

    Add doctest

--- norpc/src/lib.rs
+++ norpc/src/lib.rs
@@ -1,5 +1,31 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
+//! norpc is a library to implement in-process microservices.
+//!
+//! ```
+//! #[norpc::service]
+//! trait HelloWorld {
+//!    fn hello(s: String) -> String;
+//! }
+//! struct HelloWorldApp;
+//! #[async_trait::async_trait]
+//! impl HelloWorld for HelloWorldApp {
+//!    async fn hello(&self, s: String) -> String {
+//!        format!("Hello, {}", s)
+//!    }
+//! }
+//! let rep = tokio_test::block_on(async {
+//!     use norpc::runtime::tokio::*;
+//!     let app = HelloWorldApp;
+//!     let builder = ServerBuilder::new(HelloWorldService::new(app));
+//!     let (chan, server) = builder.build();
+//!     tokio::spawn(server.serve());
+//!     let mut cli = HelloWorldClient::new(chan);
+//!     cli.hello("World".to_owned()).await
+//! });
+//! assert_eq!(rep, "Hello, World");
+//! ```
+
 #[doc(hidden)]
 pub use async_trait::async_trait;
 #[doc(hidden)]
